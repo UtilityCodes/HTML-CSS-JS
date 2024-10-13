4a65ec921471195a0eaf5881e305c42269c93cdc
Utility Software <139227971+UtilityCodes@users.noreply.github.com>
2024-10-13 10:49:51 +0300

    Add files via upload

---
+++ showHide/script.js
@@ -0,0 +1,8 @@
+const togglePasswordButton = document.getElementById('toggle-password');
+const passwordInput = document.getElementById('password');
+
+togglePasswordButton.addEventListener('click', function () {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    this.textContent = type === 'password' ? 'Show' : 'Hide';
+});
